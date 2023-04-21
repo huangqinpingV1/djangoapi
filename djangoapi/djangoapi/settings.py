36618a722c66ffe8dde049572f4e424b3fa0440b
huangqinpingV1 <huangqinping4@gmai.com>
2023-04-21 16:13:40 +0800

    配置数据库和创建用户（组）模型

--- djangoapi/djangoapi/settings.py
+++ djangoapi/djangoapi/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'djangoapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'restjwt',
+        'USER':'adminhqp',
+        'PASSWORD':'hello123+',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
